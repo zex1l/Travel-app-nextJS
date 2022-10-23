cc410ae57699b58150173d0288151c9d5cd890be
Maks <krivorukov2007@mail.ru>
2022-10-23 15:35:33 +0300

    initialize project, first commit

---
+++ app/common/footer/Footer.jsx
@@ -0,0 +1,44 @@
+import styles from './Footer.module.scss'
+import {useRouter} from 'next/router'
+
+const navItems = [
+    {
+        link: '/',
+        icon: 'home'
+    },
+    {
+        link: '/explore',
+        icon: 'explore'
+    },
+    {
+        link: '/place',
+        icon: 'place'
+    },
+    {
+        link: '/person',
+        icon: 'person'
+    },
+]
+
+const Footer = () => {
+
+    const {push, pathname} = useRouter()
+
+    return (
+        <footer className={styles.footer}>
+            <nav>
+                {navItems.map((item, index) => 
+                    <button  
+                        className={pathname === item.link ? styles.active : ''}
+                        onClick={() => push(item.link)} 
+                        key={index}
+                    >
+                        <span className='material-icons-outlined'>{item.icon}</span>
+                    </button>
+                )}
+            </nav>
+        </footer>
+    );
+};
+
+export default Footer;
